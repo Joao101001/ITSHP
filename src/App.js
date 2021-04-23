31e48112b7ee906400392fe5115d196360675b34
Joao Alberto Navarro Sanchez <jans101001@hotmail.com>
2021-04-23 14:37:54 -0400

    Adding

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+import { useReducer, useEffect, useState, Fragment } from 'react'
+
 import './App.css';
+import Data from './Data'
+import Reducer from './Reducers/Trafficlight'
 
 function App() {
+  const [trafficlights, dispatch] = useReducer(Reducer, Data)
+
+  const lightDurations = [10000, 20, 10];
+
+  const [trafficlightIndex, setTrafficlightIndex] = useState(0);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      dispatch({ trafficlightId: trafficlightIndex })
+      setTrafficlightIndex((trafficlightIndex + 1) % 3);
+    }, lightDurations[trafficlightIndex]);
+    return () => {
+      clearTimeout(timer);
+    };
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+     {
+       trafficlights.length > 0 && (
+        <div className='container'>
+          <div className='trafficlight-box'>
+            {
+              trafficlights.map((trafficlight) => {
+                 return <p key={trafficlight.id} id='trafficlight' style={{backgroundColor: trafficlight.light, opacity: trafficlightIndex === trafficlight.id ? 1 : 0.3, boxShadow: trafficlightIndex === trafficlight.id ? '1px 1px 10px rgba(204, 204, 204, 0.5), -1px -1px 10px rgba(204, 204, 204, 0.5)' : null}} >
+                 </p>
+              })
+            }
+          </div>
+        </div>
+      )
+    }
+    </Fragment>
   );
 }
 
